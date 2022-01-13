24f4db8ace0d72d4da349735047b5ffac327d2fb
Kamil Otyński <Kamcio99@interia.com>
2022-01-13 20:47:56 +0100

    RegisterDto validation

--- src/main/java/pl/saqie/SimpleBank/app/user/controller/RegisterController.java
+++ src/main/java/pl/saqie/SimpleBank/app/user/controller/RegisterController.java
@@ -3,6 +3,7 @@ package pl.saqie.SimpleBank.app.user.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,11 +13,13 @@ import pl.saqie.SimpleBank.app.user.exception.UserIncompatibilePasswordsExceptio
 import pl.saqie.SimpleBank.app.user.model.dto.RegisterDto;
 import pl.saqie.SimpleBank.app.user.service.RegisterService;
 
+import javax.validation.Valid;
+
 @Controller
 @AllArgsConstructor
 public class RegisterController {
 
-    private final RegisterService registerService; // -> To jest interfejs
+    private final RegisterService registerService;
 
     @GetMapping("/register")
     public String getRegisterForm(Model model){
@@ -25,8 +28,15 @@ public class RegisterController {
     }
 
     @PostMapping("/register")
-    public String processRegisterUser(@ModelAttribute RegisterDto registerDto) throws UserAlreadyExistsException, UserIncompatibilePasswordsException, UserFieldValidationFailedException {
-        registerService.addNewUser(registerDto);
+    public String processRegisterUser(@ModelAttribute @Valid RegisterDto registerDto, BindingResult bindingResult, Model model) {
+        if (!bindingResult.hasErrors()) {
+            try {
+                registerService.addNewUser(registerDto);
+                model.addAttribute("registerSuccessfull", "Zostałeś pomyślnie zarejestrowany");
+            } catch (UserAlreadyExistsException | UserFieldValidationFailedException | UserIncompatibilePasswordsException e) {
+                model.addAttribute("error", e.getMessage());
+            }
+        }
         return "register";
     }
 }
